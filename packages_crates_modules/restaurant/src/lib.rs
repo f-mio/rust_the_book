d72800056d5fb5ca8e8b257cc46d4b18d0faaf44
F.Mio <52245444+f-mio@users.noreply.github.com>
2025-12-18 17:42:13 +0900

    パッケージ等の管理セクション

---
+++ packages_crates_modules/restaurant/src/lib.rs
@@ -0,0 +1,36 @@
+mod front_of_house;
+
+pub use crate::front_of_house::hosting;
+
+pub fn eat_at_restaurant() {
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+}
+
+// section 7.4
+// mod front_of_house {
+//     pub mod hosting {
+//         pub fn add_to_waitlist() {}
+
+//         fn seat_at_table() {}
+//     }
+
+//     mod serving {
+//         fn take_order() {}
+
+//         fn serve_order() {}
+
+//         fn take_payment() {}
+//     }
+// }
+
+// use crate::front_of_house::hosting;
+
+// pub fn eat_at_restaurant() {
+//     // absolute path
+//     crate::front_of_house::hosting::add_to_waitlist();
+
+//     // relative path
+//     hosting::add_to_waitlist();
+// }
